5f28a1c45d487b4f91ec78cf81adacd16f987e86
peerawet <peerawet1996@gmail.com>
2023-12-20 16:14:48 +0700

    feat/styling genres

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 import movies from "./data/movies";
 
@@ -5,23 +6,28 @@ function App() {
   return (
     <div className="App">
       <section className="movie-list-section">
-        {movies.map((movie) => {
-          return (
-            <div className="movie-box">
-              <div>
-                <img src={movie.image} className="movie-img"></img>
-              </div>
-              <div>
-                <div>Title: {movie.title}</div>
-                <div>Year: {movie.year}</div>
-                <div>Runtime: {movie.runtime}</div>
-                <div>Genres: {movie.genres}</div>
-                <div>IMDB Rating: {movie.imdbRating}</div>
-                <div>IMDB Votes: {movie.imdbVotes}</div>
-              </div>
+        {movies.map((movie, index) => (
+          <div className="movie-box" key={index}>
+            <div>
+              <img src={movie.image} alt={movie.title} className="movie-img" />
             </div>
-          );
-        })}
+            <div>
+              <div>Title: {movie.title}</div>
+              <div>Year: {movie.year}</div>
+              <div>Runtime: {movie.runtime}</div>
+              <div className="genres-parent">
+                <div>Genres:</div>
+                {movie.genres.map((genre, genreIndex) => (
+                  <div key={genreIndex} className="geres-list">
+                    {genre}
+                  </div>
+                ))}
+              </div>
+              <div>IMDB Rating: {movie.imdbRating}</div>
+              <div>IMDB Votes: {movie.imdbVotes}</div>
+            </div>
+          </div>
+        ))}
       </section>
     </div>
   );
