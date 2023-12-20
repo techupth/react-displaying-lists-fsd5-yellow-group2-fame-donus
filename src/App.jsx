6527b42e042268570653c14b6250018721558d38
peerawet <peerawet1996@gmail.com>
2023-12-20 14:54:05 +0700

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import "./App.css";
+import movies from "./data/movies";
 
 function App() {
   return (
     <div className="App">
       <section className="movie-list-section">
-        {/* Render Movie Lists Here */}
+        {movies.map((movie) => {
+          return (
+            <div className="movie-box">
+              <div>
+                <img src={movie.image} className="movie-img"></img>
+              </div>
+              <div>
+                <div>Title: {movie.title}</div>
+                <div>Year: {movie.year}</div>
+                <div>Runtime: {movie.runtime}</div>
+                <div>Genres: {movie.genres}</div>
+                <div>IMDB Rating: {movie.imdbRating}</div>
+                <div>IMDB Votes: {movie.imdbVotes}</div>
+              </div>
+            </div>
+          );
+        })}
       </section>
     </div>
   );
